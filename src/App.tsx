79ab089fbc3f720d9e05358b2ff13fea9bc34504
fangpeidong <1062027862@qq.com>
2024-11-20 07:56:03 +0800

    feat: 页面结构划分

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import React, { useState } from 'react';
-import { Button } from 'antd';
+import React from 'react';
+import Edit from './Edit';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const addCount = () => {
-    setCount(count + 1);
-  };
   return (
     <div className="App">
-      <Button onClick={() => addCount()}>add {count}</Button>
+      <Edit />
     </div>
   );
 }
--- src/Edit/index.tsx
+++ src/Edit/index.tsx
@@ -1,7 +1,23 @@
 import React, { FC } from 'react';
+import styles from './index.module.scss';
 
 const Edit: FC = () => {
-  return <div>edit</div>;
+  return (
+    <div className={styles.container}>
+      <div style={{ backgroundColor: '#fff', height: '40px' }}>Header</div>
+      <div className={styles['content-wrapper']}>
+        <div className={styles.content}>
+          <div className={styles.left}>Left</div>
+          <div className={styles.main}>
+            <div className={styles['canvas-wrapper']}>
+              <div style={{ height: '900px' }}>画布</div>
+            </div>
+          </div>
+          <div className={styles.right}>Right</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Edit;
